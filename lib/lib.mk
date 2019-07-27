9fc52f9965795aa063fa85767111a40e3f542e47
secuflag <secuflag@ticino.com>
2019-07-27 17:52:32 +0200

    Fix casual build error
     * See here: https://github.com/codewalkerster/android_vendor_amlogic_common_gpu/issues/1

--- lib/lib.mk
+++ lib/lib.mk
@@ -100,6 +100,7 @@ endif
 $(warning "the value of BOARD_INSTALL_VULKAN is $(BOARD_INSTALL_VULKAN)")
 ifeq ($(BOARD_INSTALL_VULKAN),true)
 LOCAL_POST_INSTALL_CMD = $(hide)\
+	mkdir $(dir $(LOCAL_INSTALLED_MODULE))/../hw;\
 	ln -sf ../egl/$(notdir $(LOCAL_INSTALLED_MODULE)) $(dir $(LOCAL_INSTALLED_MODULE))/../hw/vulkan.$(TARGET_PRODUCT).so
 endif
 
