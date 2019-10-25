d9c3f3bd912c93e84b2c4c2105ac2d1b0e3398b1
Chris KIM <codewalker@hardkernel.com>
2019-10-25 18:01:39 +0900

    ODROID-N2: Fix clean build error.
    Signed-off-by: Chris KIM <codewalker@hardkernel.com>
    Change-Id: Ifb36dc073d070f56d8bde8b2876a60b33bd52c58

--- lib/lib.mk
+++ lib/lib.mk
@@ -100,7 +100,12 @@ endif
 $(warning "the value of BOARD_INSTALL_VULKAN is $(BOARD_INSTALL_VULKAN)")
 ifeq ($(BOARD_INSTALL_VULKAN),true)
 LOCAL_POST_INSTALL_CMD = $(hide)\
-	ln -sf ../egl/$(notdir $(LOCAL_INSTALLED_MODULE)) $(dir $(LOCAL_INSTALLED_MODULE))/../hw/vulkan.$(TARGET_PRODUCT).so
+	if [ ! -d $(dir $(LOCAL_INSTALLED_MODULE))/../hw ]; then \
+		mkdir -p $(dir $(LOCAL_INSTALLED_MODULE))/../hw; \
+	fi;\
+	cd $(dir $(LOCAL_INSTALLED_MODULE))/../hw;\
+	pwd; \
+	ln -sf ../egl/$(notdir $(LOCAL_INSTALLED_MODULE)) ./vulkan.$(TARGET_PRODUCT).so;
 endif
 
 include $(BUILD_PREBUILT)
